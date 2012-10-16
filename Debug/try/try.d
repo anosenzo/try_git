cd70a1193938c76914b29046bc64b3d7b516275e
Alejandro Nosenzo <alejandro_nosenzo@hotmail.com>
2012-10-15 22:56:38 -0300

    prueba de C

---
+++ Debug/try/try.d
@@ -0,0 +1,1 @@
+try/try.d: ../try/try.c
---
+++ try/try.c
@@ -0,0 +1,14 @@
+/*
+ * try.c
+ *
+ *  Created on: 15/10/2012
+ *      Author: utnso
+ */
+
+#include <stdio.h>
+
+int main(void){
+	printf("hola mundo\n");
+
+	return 0;
+}
